6204e7a456289a6320ddb3b0f452fbb6e5d21450
robi874 <62009463+robi874@users.noreply.github.com>
2022-05-17 14:21:04 +0200

    Navbar and home page creation
    Created the navbar with items, and the home/start page.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,22 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-md">
+          <a className="navbar-brand text-light" href="#">
+            <img src="https://i.pinimg.com/474x/27/14/51/2714519483a333b8a2f0277aed704b89.jpg" alt="" width="30" height="24" className="d-inline-block align-text-top" />
+            Inspirational Mems
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">My mems</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Mem generator</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container mt-5">
+        <h1 className="text-center">Inspirational mems</h1>
+        <div class="d-grid gap-2 col-1 mx-auto mt-4">
+          <button className="btn btn-dark" type="button">Start</button>
+        </div>
+      </div>
     </div>
+
   );
 }
 
