1cc5b27da57bf90bd873cc53b12d52749fb27cd2
cybertilla <bertilla.fabris@gmail.com>
2022-05-19 14:00:38 +0200

    fix img loop

--- my-app/src/pages/Meme.js
+++ my-app/src/pages/Meme.js
@@ -4,23 +4,26 @@ import React, { useEffect, useState } from 'react';
 
 const Meme = () => {
   const [image, setImage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
 
-  useEffect(() => {fetch('https://api.memegen.link/templates')
-  .then(response => response.json())
-  .then(data => {
-    let number = Math.floor(Math.random() * data.length); // Returns a random number
-    let img = data[number].blank
-    setImage(img)
-  })}, []);
+  if (image === null && loading === false) {
+    //check that loading is false (prevents the following from running on itself)
+    setLoading(true);
+    fetch('https://api.memegen.link/templates')
+    .then(response => response.json())
+    .then(data => {
+      let number = Math.floor(Math.random() * data.length); // Returns a random number
+      let img = data[number].blank
+      setImage(img)
+    });
+  }
 
   
-
-
   return (
     <div>
       <h1>Meme page</h1>
-      <img src={image} alt="Meme"></img>
+      {image === null || <img src={image} alt="Meme" />}
     </div>
 
   );
