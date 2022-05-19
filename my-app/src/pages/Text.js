f0a173aca7eeec8ac8b4ef3ca5d5f69cb8620c70
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-05-19 14:06:49 +0200

    Succesfully fetched Quote-API

--- my-app/src/pages/Text.js
+++ my-app/src/pages/Text.js
@@ -1,5 +1,28 @@
+import React, { useState } from 'react';
+
+
 const Text = () => {
-    return <h1>Text page</h1>;
-  };
-  
-  export default Text;
+
+  const [quote, setQuote] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  if (quote === null && loading === false) {
+    setLoading(true);
+    fetch('https://api.quotable.io/random')
+      .then(response => response.json())
+      .then(data => {
+        setQuote(data.content)
+      });
+  }
+
+  return (
+    <div>
+      <h1>Text page</h1>
+      {quote === null || <p>{quote}</p>}
+      
+    </div>
+
+  );
+};
+
+export default Text;
