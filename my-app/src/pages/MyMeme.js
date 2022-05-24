71b5fe4d551cbc1d3760247be62b221d9d6b35f6
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-05-24 15:01:19 +0200

    Loaded memes from localstorage in MyMemes-page
    memes displayed via bootstrap cards

--- my-app/src/pages/MyMeme.js
+++ my-app/src/pages/MyMeme.js
@@ -1,5 +1,22 @@
+import { loadMemes } from "../LocalStorage";
+
 const MyMeme = () => {
-    return <h1>My Memes Page</h1>;
-  };
-  
-  export default MyMeme;
+
+  let memes = loadMemes()
+
+  return (
+    <div>
+      {(memes !== []) ? memes.map((meme) =>
+        <div className="card mt-2" style={{width: "18rem", display: "inline-block"}}>
+           <img src={meme.url} class="card-img-top" alt="Meme"></img>
+          <div className="card-body">
+            <p>{meme.text}</p>
+          </div>
+        </div>
+      ) : <div><p>You have no memes saved</p></div>}
+    </div>
+
+  );
+};
+
+export default MyMeme;
