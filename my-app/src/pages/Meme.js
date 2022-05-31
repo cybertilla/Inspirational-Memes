9001f92f7f29d4cd7e591c0fe540bb5d0de6f4c3
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-05-31 14:01:36 +0200

    Generate meme-button added to meme-page

--- my-app/src/pages/Meme.js
+++ my-app/src/pages/Meme.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 
 
@@ -6,10 +6,24 @@ const Meme = () => {
   const [image, setImage] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    // Update the document title using the browser API
+    if (image === null && loading === false) {
 
-  if (image === null && loading === false) {
-    //check that loading is false (prevents the following from running on itself)
-    setLoading(true);
+      //check that loading is false (prevents the following from running on itself)
+      setLoading(true);
+      fetch('https://api.memegen.link/templates')
+        .then(response => response.json())
+        .then(data => {
+          let number = Math.floor(Math.random() * data.length); // Returns a random number
+          let img = data[number]
+          setImage({ "image": img.blank, "id": img.id })
+        });
+    }
+
+  }, [image, loading]);
+
+  const fetchRequest = useCallback(() => {
     fetch('https://api.memegen.link/templates')
       .then(response => response.json())
       .then(data => {
@@ -17,16 +31,22 @@ const Meme = () => {
         let img = data[number]
         setImage({ "image": img.blank, "id": img.id })
       });
-  }
+  }, []);
 
 
   return (
     <div className="container mt-4">
       {image === null ||
         <div>
-          
-          <img src={image.image} className="mx-auto d-block" alt="Meme" style={{}} />
+
+          <img src={image.image} className="mx-auto d-block" alt="Meme" />
           <p className="text-center">{image.id}</p>
+
+          <div className="d-grid gap-2 col-2 mx-auto mt-4">
+            <button className="btn btn-dark" type="button" onClick={fetchRequest}>Generate Meme</button>
+          </div>
+
+
         </div>
       }
     </div>
