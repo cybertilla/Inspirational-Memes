4679cd325f6e0f9fc97cfb270decb8492b1c23a0
cybertilla <bertilla.fabris@gmail.com>
2022-05-31 12:00:34 +0200

    bootstrap card on homepage

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -5,25 +5,32 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <div className="container mt-5">
-            <h1 className="text-center">Inspirational mems</h1>
+            <h1 className="text-center">Inspirtional mems</h1>
+            <div class="card">
+                <div class="card-body">
+                    <p className="text-center">Welcome to Inspirtional Mems, a game in which you can
+                        create the funniest combination of memes and inspirational quotes.
 
-                <p className="text-center">Welcome to Inspirational Mems, a game in which you can
-                    create the funniest combination of memes and inspirational quotes.
-                    Gather a group of players around a table and pick a meme master,
-                    that person will be the first to click "Generate Mems", while the others
-                    will head to "Generate text". The principle is similar to cards against humanity,
-                    the best combination is decided by the current meme master, the winner becomes the new meme master.
-                </p>
-                <p className="text-center">Under each meme there is a meme ID, in order to save any meme combo click on a quote,
-                    type in the meme ID to match your quote and click on "Save meme". Now you can see it in the My mems page!
-                </p>
+                        Gather a group of players around a table and pick a <i>meme master</i>,
+                        that person will be the first to click "Generate Mems", while the others
+                        will head to "Generate text". The principle is similar to Cards Against Humanity,
+                        the best/worst combination is decided by the current <i>meme master</i>,
+                        the winner becomes the new <i>meme master</i> for the next round.
+                    </p>
+                    <p className="text-center">Under each meme there is a memeID, in order to save any meme+quote combo click on a quote,
+                        type in the memeID to match your quote and click on "Save meme". Now you can see it in the My mems page!
+                    </p>
+                </div>
+            </div>
+            <div>
+                <br/>
                 <h2 className="text-center">Start Game!</h2>
                 <div className="d-grid gap-2 col-2 mx-auto mt-4">
-                <button className="btn btn-dark" type="button"><Link style={{textDecoration: "none", color:"white"}} to="/Meme">Generate Meme</Link> </button>
-                
-            </div>
-            <div className="d-grid gap-2 col-2 mx-auto mt-4">
-                <button className="btn btn-dark" type="button"><Link style={{textDecoration: "none", color:"white"}} to="/Text">Generate Text</Link></button>
+                    <button className="btn btn-dark" type="button"><Link style={{ textDecoration: "none", color: "white" }} to="/Meme">Generate Meme</Link> </button>
+                </div>
+                <div className="d-grid gap-2 col-2 mx-auto mt-4">
+                    <button className="btn btn-dark" type="button"><Link style={{ textDecoration: "none", color: "white" }} to="/Text">Generate Text</Link></button>
+                </div>
             </div>
         </div>
 
