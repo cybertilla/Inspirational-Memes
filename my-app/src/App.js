eb9701357b75dcdff90c1f97d1a671b0413a6b34
robi874 <62009463+robi874@users.noreply.github.com>
2022-06-03 14:35:50 +0200

    Fixed the Link tag on the navbar brand

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,18 +12,19 @@ function App() {
     <BrowserRouter>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-md">
-          <a className="navbar-brand text-light" href="/">
-            <Link style={{ textDecoration: "none", color: "white" }} to="/">
-            </Link>
-            <img src="https://i.pinimg.com/474x/27/14/51/2714519483a333b8a2f0277aed704b89.jpg" alt="" width="30" height="24" className="d-inline-block align-text-top" />
-            Inspirational Mems
-          </a>
+          <Link style={{ textDecoration: "none", color: "white" }} to="/">
+            <a className="navbar-brand text-light">
+
+              <img src="https://i.pinimg.com/474x/27/14/51/2714519483a333b8a2f0277aed704b89.jpg" alt="" width="30" height="24" className="d-inline-block align-text-top" />
+              Inspirational Mems
+            </a>
+          </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto ">
-            <li className="nav-item">
+              <li className="nav-item">
                 <Link style={{ textDecoration: "none", color: "white" }} to="/MyMeme">
                   <span className="nav-link">My mems</span>
                 </Link>
